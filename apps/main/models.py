d0929442da834d37f5a8788a99a859958400e041
Gisel Calvo <gisel.calvo@gmail.com>
2017-08-23 15:50:39 -0700

    added model

--- apps/main/models.py
+++ apps/main/models.py
@@ -3,3 +3,15 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
+class User(models.Model):
+	FB_id = models.IntegerField()
+	STRA_id = models.CharField(max_length=255, null=True)
+	STRA_accessToken = models.TextField(null=True)
+	STRA_pic = models.CharField(max_length=255, null=True)
+	user_friends = models.ManyToManyField('self', related_name='friends_friends')
+	user_likes = models.ManyToManyField('self', related_name='liked_by')
+
+
+
--- apps/main/views.py
+++ apps/main/views.py
@@ -5,4 +5,14 @@ from django.utils.crypto import get_random_string
 import bcrypt
 
 def index(request):
+
 	return render(request, "main/index.html")
+
+# #if user not in database
+# def create_user(request):
+
+# 	#check if user is database
+# 	user = User.objects.get(FB_id=request.session['FB_id'])
+
+# 	#if not create user
+# 	user = User.objects.create(FB_id=request.session['FB_id'])
