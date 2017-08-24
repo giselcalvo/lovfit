3c796ff13a63dcf8f6d5697eb5fbcb048ed9682c
Gisel Calvo <gisel.calvo@gmail.com>
2017-08-23 18:50:15 -0700

    added create_user

--- apps/main/models.py
+++ apps/main/models.py
@@ -3,9 +3,25 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class UserManager(models.Manager):
+	
+	def user_validator(self, postData):
+		error = {}
+		print postData
+		#check if user exist in the database
+		if len(User.objects.filter(FB_id=postData['FB_id'])) == 0:
+			print "creating new user: ", postData['FB_id']
+			user = User.objects.create(FB_id=postData['FB_id'])
+		else:
+			error['user'] = "User is already in the database"
+		if error:
+			return error	
+		return user
+	
 
 
 class User(models.Model):
+	#first_name = models.CharField(max_length=255)
 	FB_id = models.IntegerField()
 	STRA_id = models.CharField(max_length=255, null=True)
 	STRA_accessToken = models.TextField(null=True)
@@ -13,5 +29,5 @@ class User(models.Model):
 	user_friends = models.ManyToManyField('self', related_name='friends_friends')
 	user_likes = models.ManyToManyField('self', related_name='liked_by')
 
-
+	objects = UserManager()
 
--- apps/main/views.py
+++ apps/main/views.py
@@ -4,6 +4,7 @@ from time import gmtime, strftime
 from django.utils.crypto import get_random_string
 from django.views.decorators.csrf import csrf_exempt
 import bcrypt, json
+from models import User
 
 def index(request):
 
@@ -15,7 +16,18 @@ def create_user(request):
 	print "I am in create user"
 	data = json.loads(request.POST['content'])
 	print data['FB_id']
-	print data['friends'][0]['name']
+	print data['friends'][0]['name']	
+	request.session['id'] = data['FB_id']
+
+	result = User.objects.user_validator(data)
+
+	print "session: ", request.session['FB_id']
+
+	#print "sessions:", request.session['FB_id']
+
+
+	#Check if user exist in database
+
 
 	# for key, item in request.POST.iteritems() :
 	# 	if key == 'FB_id' :
